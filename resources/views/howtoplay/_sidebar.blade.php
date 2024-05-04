7c33eef3d0a142d4bd3f7e149ec187d98be3387d
Rimworldmachine\Shaine <shainareeves102@gmail.com>
2024-05-04 14:45:48 -0230

    no message

--- resources/views/howtoplay/_sidebar.blade.php
+++ resources/views/howtoplay/_sidebar.blade.php
@@ -15,38 +15,4 @@ ul>
             <div class="sidebar-item"><a href="{{ url('gallery/all') }}" class="{{ set_active('gallery/all') }}">All Recent Submissions</a></div>
         </li>
     @endif
-
-    @if ($galleryPage && $sideGallery->children->count())
-        <li class="sidebar-section">
-            <div class="sidebar-section-header">{{ $sideGallery->name }}: Sub-Galleries</div>
-            @foreach ($sideGallery->children()->visible()->get() as $child)
-                <div class="sidebar-item"><a href="{{ url('gallery/' . $child->id) }}" class="{{ set_active('gallery/' . $child->id) }}">{{ $child->name }}</a></div>
-            @endforeach
-        </li>
-    @endif
-
-    @if ($galleryPage && $sideGallery->siblings() && $sideGallery->siblings->count())
-        <li class="sidebar-section">
-            <div class="sidebar-section-header">{{ $sideGallery->parent->name }}: Sub-Galleries</div>
-            @foreach ($sideGallery->siblings()->visible()->get() as $sibling)
-                <div class="sidebar-item"><a href="{{ url('gallery/' . $sibling->id) }}" class="{{ set_active('gallery/' . $sibling->id) }}">{{ $sibling->name }}</a></div>
-            @endforeach
-        </li>
-    @endif
-
-    @if ($galleryPage && $sideGallery->avunculi() && $sideGallery->avunculi->count())
-        <li class="sidebar-section">
-            <div class="sidebar-section-header">{{ $sideGallery->parent->parent->name }}: Sub-Galleries</div>
-            @foreach ($sideGallery->avunculi()->visible()->get() as $avunculus)
-                <div class="sidebar-item"><a href="{{ url('gallery/' . $avunculus->id) }}" class="{{ set_active('gallery/' . $avunculus->id) }}">{{ $avunculus->name }}</a></div>
-            @endforeach
-        </li>
-    @endif
-
-    <li class="sidebar-section">
-        <div class="sidebar-section-header">Galleries</div>
-        @foreach ($sidebarGalleries as $gallery)
-            <div class="sidebar-item"><a href="{{ url('gallery/' . $gallery->id) }}" class="{{ set_active('gallery/' . $gallery->id) }}">{{ $gallery->name }}</a></div>
-        @endforeach
-    </li>
 </ul>
