6c1b1b02d61d92c1de3eca12e7e35dd3088123a6
Rimworldmachine\Shaine <shainareeves102@gmail.com>
2024-05-04 14:37:08 -0230

    no message

--- resources/views/howtoplay/_sidebar.blade.php
+++ resources/views/howtoplay/_sidebar.blade.php
@@ -1,9 +1,9 @@
 ul>
-    <li class="sidebar-header"><a href="{{ url('howtoplay') }}" class="card-link">Gallery</a></li>
+    <li class="sidebar-header"><a href="{{ url('howtoplay') }}" class="card-link">How To Play</a></li>
 
     @auth
         <li class="sidebar-section">
-            <div class="sidebar-section-header">My Submissions</div>
+            <div class="sidebar-section-header">Getting Started</div>
             <div class="sidebar-item"><a href="{{ url('gallery/submissions/pending') }}" class="{{ set_active('gallery/submissions*') }}">My Submission Queue</a></div>
             <div class="sidebar-item"><a href="{{ url('user/' . Auth::user()->name . '/gallery') }}" class="{{ set_active('user/' . Auth::user()->name . '/gallery') }}">My Gallery</a></div>
             <div class="sidebar-item"><a href="{{ url('user/' . Auth::user()->name . '/favorites') }}" class="{{ set_active('user/' . Auth::user()->name . '/favorites') }}">My Favorites</a></div>
