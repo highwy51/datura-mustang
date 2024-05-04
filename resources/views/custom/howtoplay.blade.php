9506bff7df034d741f5185eb1107674e625dcc1e
Rimworldmachine\Shaine <shainareeves102@gmail.com>
2024-05-04 14:06:22 -0230

    no message

--- resources/views/custom/howtoplay.blade.php
+++ resources/views/custom/howtoplay.blade.php
@@ -1,16 +1,11 @@
 @extends('layouts.app')
 
 
-@section('title') HowToPlay @endsection
+@section('title') How To Play @endsection
 
 
 @section('content')
-{!! breadcrumbs(['How To Play' => 'HTP']) !!}
-
-
-<div class="site-page-content parsed-text">
-    {!! $page->parsed_text !!}
-</div>
+{!! breadcrumbs(['How To Play' => 'howtoplay']) !!}
 
 
 @endsection
