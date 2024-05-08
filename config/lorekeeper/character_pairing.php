b7f8ebe17862c65523fdadfbde2986ff81675b86
Rimworldmachine\Shaine <shainareeves102@gmail.com>
2024-05-08 15:48:20 -0230

    no message

--- config/lorekeeper/character_pairing.php
+++ config/lorekeeper/character_pairing.php
@@ -4,13 +4,13 @@ return [
     // Number between 0-100. Percentage chance to inherit traits from both parents upon generating offspring. Set 100 for always. Set 0 for never.
     'trait_inheritance' => 100,
     // 0: Any character can be paired. 1: Only male/female characters can be paired.
-    'sex_restriction' => 0,
+    'sex_restriction' => 1,
     // 0: Disabled aka characters do not need a set sex. 1: Enabled aka characters must have a set sex
-    'force_sex' => 0,
+    'force_sex' => 1,
     // 0: Disabled, do not roll sex. 1-100: Chance to generate a male offspring. Must total 100 with the pairing_female_percentage setting.
-    'offspring_male_percentage' => 0,
+    'offspring_male_percentage' => 50,
     // 0: Disabled, do not roll sex. 1-100: Chance to generate a male offspring. Must total 100 with the pairing_male_percentage setting.
-    'offspring_female_percentage' => 0,
+    'offspring_female_percentage' => 50,
     // 0: Disabled. Number of days to wait between pairing a character.
     'cooldown' => 0,
     // 0: rarity is random between rarity options from features, with boosts affecting character rarity
