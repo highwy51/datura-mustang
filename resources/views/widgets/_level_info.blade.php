2f673bd5cf3fd84f4079bd2a1923b92b8b6bbcd1
daturamustang <shainareeves102@gmail.com>
2024-07-02 12:12:14 -0230

    Changing Level syntax to read Age instad

--- resources/views/widgets/_level_info.blade.php
+++ resources/views/widgets/_level_info.blade.php
@@ -1,14 +1,14 @@
 <div class="card mb-3">
     <div class="card-header h2">
-        Level Information
+        Age Information
         <span class="badge badge-{{ $level->nextLevel ? 'dark' : 'success' }} text-white mx-1 float-right" data-toggle="tooltip" title="Level {{ $level->current_level }}">
-            {{ $level->nextLevel ? 'Current Lvl: ' . $level->current_level : 'Max Level' }}
+            {{ $level->nextLevel ? 'Current Age: ' . $level->current_level : 'Max Age' }}
         </span>
     </div>
     <div class="card-body">
         <div class="container text-center mb-3">
             @if ($level->nextLevel)
-                <p><b>Next Level:</b> {{ $level->nextLevel->level }}</p>
+                <p><b>Next Age:</b> {{ $level->nextLevel->level }}</p>
                 {{ $level->current_exp }}/{{ $level->nextLevel->exp_required }}
                 <div class="progress">
                     <div class="progress-bar progress-bar-striped active progress-bar-animated" role="progressbar" aria-valuenow="{{ $level->current_exp }}" aria-valuemin="0" aria-valuemax="{{ $level->nextLevel->exp_required }}"
@@ -19,17 +19,17 @@
                 @if ($level->current_exp >= $level->nextLevel->exp_required && Auth::check() && ($level->user ?? Auth::user()->id == $level->character?->id))
                     <div class="text-center m-1">
                         <b>
-                            <p>You have enough EXP to advance to the next level!</p>
+                            <p>You have enough AP to Age up!</p>
                         </b>
                     </div>
                     {!! Form::open(['url' => $level->user ? '/stats/level' : $level->character->url . '/stats/level']) !!}
 
-                    {!! Form::submit('Level up!', ['class' => 'btn btn-success mb-2']) !!}
+                    {!! Form::submit('Age up!', ['class' => 'btn btn-success mb-2']) !!}
 
                     {!! Form::close() !!}
                 @endif
             @else
-                {{ $level->current_exp }} Exp (Max Level)
+                {{ $level->current_exp }} AP (Max Age)
                 <div class="progress">
                     <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="{{ $level->current_exp }}" aria-valuemin="0" aria-valuemax="{{ $level->current_exp }}" style="width:100%">
                         {{ $level->current_exp }}
