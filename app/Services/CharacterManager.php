261ca0f5b98c2b3e3e8f47d0f4e3ad2c7024b5e2
daturamustang <shainareeves102@gmail.com>
2024-07-05 13:42:45 -0230

    Name on creation working finally

--- app/Services/CharacterManager.php
+++ app/Services/CharacterManager.php
@@ -2256,7 +2256,7 @@ class CharacterManager extends Service {
                 'sale_value', 'transferrable_at', 'is_visible',
             ]);
 
-            $characterData['name'] = ($isMyo && isset($data['name'])) ? $data['name'] : null;
+            $characterData['name'] = (isset($data['name'])) ? $data['name'] : null;
             $characterData['owner_url'] = isset($characterData['user_id']) ? null : $data['owner_url'];
             $characterData['is_sellable'] = isset($data['is_sellable']);
             $characterData['is_tradeable'] = isset($data['is_tradeable']);
